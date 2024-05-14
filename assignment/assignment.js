d3928c6a5ed898c2133c362f85f561da7e5a217f
Abraham Joseph <88972937+Abtanjoe@users.noreply.github.com>
2024-05-13 22:38:03 -0800

    Add files via upload

---
+++ assignment/assignment.js
@@ -0,0 +1,13 @@
+const viewButton = document.querySelector(".viewBtn");
+const hideButton = document.querySelector(".hideBtn");
+const cont = document.querySelector(".contents");
+const viewref = document.querySelector(".viewref");
+const ref = document.querySelector(".ref");
+
+const viewCv = () => {
+    cont.style.display = "flex";
+}
+
+const hideCv = () => {
+    cont.style.display = "none";
+}
